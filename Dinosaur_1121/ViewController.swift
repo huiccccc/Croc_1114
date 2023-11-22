321bde5317c8471648b0239ecfd688d47244455d
hui <huichung9509@gmail.com>
2023-11-22 13:42:19 +0800

    Initial Commit

---
+++ Dinosaur_1121/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Demo_1114
+//
+//  Created by 莊惠中 on 2023/11/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
